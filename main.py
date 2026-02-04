7d8bfcbb1e824097a326c2bf22fd9fe681a10ab0
stella9921 <stella9921@gmail.com>
2026-02-04 11:07:40 +0000

    ATO 비헤시안 비교기법 추가 main.py and pdt_strategies.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from src.data.dataloader import get_dataloaders
 from src.models import get_model, get_prune_fn
 from src.pruning.sensitivity import maybe_load_or_compute_sensitivity
 from src.pruning.pat_strategies import PATPruner
-from src.pruning.pdt_strategies import PDTPruner,HAPPruner,SNOWSPruner
+from src.pruning.pdt_strategies import PDTPruner,HAPPruner,SNOWSPruner, ATOPruner
 
 def analyze_topology_and_profiling(model, device, config, tag="Before Pruning"):
     """Stage 1(Topology)과 Stage 4(Resource)를 동시에 분석"""
@@ -139,6 +139,9 @@ def execute_pdt_experiment(model, config, train_loader, val_loader, test_loader,
     elif strategy_type == 'snows':
         pdt_engine = SNOWSPruner(model, config, args, topology_groups)
         print("[System] Initializing SNOWSPruner (Pure Hessian Trace Strategy)")
+    elif strategy_type == 'ato': # 추가
+        pdt_engine = ATOPruner(model, config, args, topology_groups)
+        print("[System] Initializing ATOPruner (Magnitude Strategy)")
     else:
         pdt_engine = PDTPruner(model, config, args, topology_groups)
         print("[System] Initializing PDTPruner (Proposed Grad-EMA Strategy)")
